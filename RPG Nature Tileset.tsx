424f64e07907311991dcadf3583b7d6397542969
Drachensegler <github@drachensegler.kloenk.de>
2021-01-24 15:34:15 +0100

    Beach construction

--- RPG Nature Tileset.tsx
+++ RPG Nature Tileset.tsx
@@ -1,4 +1,20 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="RPG Nature Tileset" tilewidth="32" tileheight="32" tilecount="180" columns="20">
  <image source="../../Tiled/RPG Nature Tileset.png" width="641" height="288"/>
+ <terraintypes>
+  <terrain name="erdweg" tile="0"/>
+ </terraintypes>
+ <tile id="125" terrain=",,,0"/>
+ <tile id="126" terrain=",,0,0"/>
+ <tile id="127" terrain=",,0,"/>
+ <tile id="128" terrain=",,,0"/>
+ <tile id="129" terrain=",,0,"/>
+ <tile id="145" terrain=",0,,0"/>
+ <tile id="146" terrain="0,0,0,0"/>
+ <tile id="147" terrain="0,,0,"/>
+ <tile id="148" terrain=",0,,"/>
+ <tile id="149" terrain="0,,,"/>
+ <tile id="165" terrain=",0,,"/>
+ <tile id="166" terrain="0,0,,"/>
+ <tile id="167" terrain="0,,,"/>
 </tileset>
