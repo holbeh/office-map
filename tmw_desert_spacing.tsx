47fd04fafd9956827d87dba05cc19afb484823f5
Drachensegler <github@drachensegler.kloenk.de>
2021-01-24 16:06:16 +0100

    fix tilesets import

--- tmw_desert_spacing.tsx
+++ tmw_desert_spacing.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="tmw_desert_spacing" tilewidth="32" tileheight="32" spacing="1" tilecount="48" columns="8">
- <image source="C:/Program Files/Tiled/examples/tmw_desert_spacing.png" width="265" height="199"/>
+ <image source="tmw_desert_spacing.png" width="265" height="199"/>
  <terraintypes>
   <terrain name="Neues Terrain" tile="15"/>
  </terraintypes>
